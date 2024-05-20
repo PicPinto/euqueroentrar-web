b5bff8ced60b237d7cbe24825fdb22501e5688e1
Fixo <picpinto2022@gmail.com>
2024-05-20 03:01:21 +0100

    Footer added

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2024 euqueroentrar. <br />Todos os direitos reservados.</p>
+        </footer>
+    );
+}
+
+export default Footer;
